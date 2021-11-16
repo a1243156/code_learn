0caf31f7ff9b6a1089e2678da271cef8037db205
196559919@qq.com <196559919@qq.com>
2021-11-16 18:56:16 +0800

    多线程

--- 114zr.py
+++ 114zr.py
@@ -1,4 +1,5 @@
 import re
+import time
 import queue
 import threading
 import requests
@@ -20,7 +21,7 @@ def save(txt):
     with open('neir.txt','a') as f:
         f.write(txt + '\n')
 
-def crawl_txt(endid):
+def crawl_txt(endid,page):
     url2 = 'http://www.114zr.com/lost_display.php?id={}'.format(endid)
     res_detial = requests.get(url2).text
     re_list = ['<td height="38">师傅姓名:(.*?)&','; 性别:(.*?)<','<td height="38">工作地点:(.*?)&','; 工种类型:(.*?)<',"style='vertical-align:text-bottom' >:(.*?)<",'<td height="120" valign="top">([\s\S]*?)</td>','<td height="38">联系电话:([\s\S]*?)&','电子邮箱:([\s\S]*?)<br>']
@@ -30,7 +31,7 @@ def crawl_txt(endid):
         find_detial_list.append(find_detial)
     find_detial_list.append(url2)
     save("|".join(find_detial_list))
-    print("打印完成",endid)
+    print(page,"打印完成",endid)
 
 
 
@@ -39,12 +40,30 @@ def crawl_pageurl(page):
     res = requests.get(url).text
     find_endid = re.findall(r'<a href="lost_display.php\?id=(.*?)">',res)
     for endid in find_endid:
-        crawl_txt(endid)
+        crawl_txt(endid,page)
 
 
 #单线程
-# for page in range(1,65):
+# t1 = time.time()
+# for page in range(1,10):
 #     crawl_pageurl(page)
 #     print('-----第{}页采集完毕------'.format(page))
+# print('总用时',time.time()-t1)
 
-#
+#多线程
+if __name__ == '__main__':
+    t1 = time.time()
+    queue = queue.Queue()
+
+    for page in range(1,10):
+        queue.put(page)
+
+    print ('队列已完成填充')
+
+    for i in range(5):
+        work = Mythread(queue=queue)
+        work.daemon = True
+        work.start()
+
+    queue.join()
+    print('总用时',time.time()-t1)
